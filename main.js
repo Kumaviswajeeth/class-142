c35f20eee90155f88398d447f5ec1154aed79755
Kumaviswajeeth <93716307+Kumaviswajeeth@users.noreply.github.com>
2023-01-21 17:25:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+noseX = "";
+noseY  = "";
+marioX = "";
+marioY = "";
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+canvas = createCanvas(1240,336);
+canvas.parent('canvas');
+
+instializeInSetup(mario);
+
+video = createCapture(VIDEO);
+video.size(800,400);
+video.parent('game_console');
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.X;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function modelLoaded()
+{
+	console.log('Model Loaded!');
+}
+
+function draw() 
+{
+ game()
+}
+
+
+
+
+
+
